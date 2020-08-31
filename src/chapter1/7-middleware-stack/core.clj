2ef83cd9d2c370f35485e06123821e11e6000d81
meamruri <meammf@yandex.ru>
2020-08-31 14:27:05 +0300

    Add custom middleware for describing pattern with middleware stack

--- src/chapter1/7-middleware-stack/core.clj
+++ src/chapter1/7-middleware-stack/core.clj
@@ -1,7 +1,8 @@
 (ns core
   (:require [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.params :refer [wrap-params]]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]]
+            [taoensso.timbre :as timbre]))
 
 (defn app-naked [request]
   (let [who (get-in request [:params :who])]
@@ -9,9 +10,17 @@
      :headers {"Content-Type" "text/plain"}
      :body (format "Hello %s" who)}))
 
+;; middleware for actual parameters logging
+(defn with-log-parameters [handler]
+  (fn [request]
+    (let [params (get request :params)]
+      (timbre/info params)) ;; Fixme: twice logging - with actual parameter and then with empty map
+    (handler request)))
+
 (def wrap-params* (comp wrap-params wrap-keyword-params))
 
 (def app (-> app-naked
+             with-log-parameters ;; this custom middleware will be called before app-naked but after `wrap-params*`
              wrap-params*))
 
 (def server (atom nil))
