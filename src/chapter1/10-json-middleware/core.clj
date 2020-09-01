c23d85ab6d50f57e2acfa85d85d13c875dbc543d
meamruri <meammf@yandex.ru>
2020-09-01 11:40:41 +0300

    Finishing up core example

--- src/chapter1/10-json-middleware/core.clj
+++ src/chapter1/10-json-middleware/core.clj
@@ -9,9 +9,9 @@
 (def wrap-params+ (comp wrap-params wrap-keyword-params))
 
 (def users (atom {:1 {:name "woofer"
-                      :email "woofer@gmail.com"}}
-                 :2 {:name "meower"
-                     :email "meower@gmail.com"}))
+                      :email "woofer@gmail.com"}
+                  :2 {:name "meower"
+                      :email "meower@gmail.com"}}))
 
 (defn get-all-users [_]
   {:status 200
@@ -28,24 +28,35 @@
               :error-message "User not found"}})))
 
 (defn handle-user-change [request]
-  {})
+  (let [user-id (-> request :params :id keyword)]
+    (if-let [_ (user-id @users)]
+      {:status 409
+       :body "Already exist"}
+      (let [{:keys [body]} request
+            {:keys [name email]} body
+            user {:name name :email email}]
+        (timbre/info (format "User for create is %s" body))
+        (reset! users (assoc-in @users [user-id] user))
+        {:status 201
+         :body user}))))
 
 (defroutes app-routes
   (context "/users" []
     (GET "/" request (get-all-users request))
     (context "/:id" [_]
       (GET "/" request (handle-get-user request))
-      (POST "/" request ((handle-user-change (keyword :1)) request)))))
+      (POST "/" request (handle-user-change request)))))
 
 
 (def app (-> app-routes             
              wrap-params+
-             wrap-json-body
+             (wrap-json-body {:keywords? true})
              wrap-json-response))
 
 (def server (atom nil))
 
 (defn start []
+  (timbre/info "Starting server for clojure rock")
   (reset! server (run-jetty app {:port 8080 :join? false})))
 
 (defn stop []
