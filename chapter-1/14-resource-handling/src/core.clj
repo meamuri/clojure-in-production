c829c5c107643f5f6562b485c35e057cf2006b34
meamruri <meammf@yandex.ru>
2020-09-02 12:34:16 +0300

    Add mime types description for image request

--- chapter-1/14-resource-handling/src/core.clj
+++ chapter-1/14-resource-handling/src/core.clj
@@ -1,11 +1,15 @@
 (ns core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [ring.middleware.resource :refer [wrap-resource]]))
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.content-type :refer [wrap-content-type]]))
 
 (defn handler [request] {:status 200})
 
 ;; Go to address localhost:8080/public.html for looking at resource serving result
-(def app (wrap-resource handler "public"))
+(def app (-> (wrap-resource handler "public")
+             (wrap-content-type 
+              {:mime-types
+               {"png" "image/png"}})))
 
 (def server (atom nil))
 
