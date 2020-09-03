8243d36a2bcfb5d7e192c85eddd598d1a24d6ccf
meamruri <meammf@yandex.ru>
2020-09-03 18:48:43 +0300

    Add age + url spec examples

--- chapter-2/01-basics/src/core.clj
+++ chapter-2/01-basics/src/core.clj
@@ -14,3 +14,21 @@
   (s/valid? ::ne-string 1)       ;; => false
   (s/valid? ::ne-string "")      ;; => false
   (s/valid? ::ne-string "test")) ;; => true
+
+(s/def ::url
+  (s/and ::ne-string
+       (partial re-matches #"(?i)^http(s?)://.*")))
+
+(comment
+  (s/valid? ::url nil)                  ;; => false
+  (s/valid? ::url "test")               ;; => false
+  (s/valid? ::url "http://peace.com"))  ;; => true
+
+(s/def ::age
+  (s/and int?
+         #(<= 0 % 150)))
+
+(comment
+  (s/valid? ::age nil)       ;; => false
+  (s/valid? ::age "test")    ;; => false
+  (s/valid? ::age 1))        ;; => true
