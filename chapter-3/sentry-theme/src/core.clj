c98d38c8c23bebaf0d0b5a0b698e3a3f528f87e7
meamruri <meammf@yandex.ru>
2020-10-04 16:30:45 +0300

    Add sentry example

--- chapter-3/sentry-theme/src/core.clj
+++ chapter-3/sentry-theme/src/core.clj
@@ -1,3 +1,22 @@
-(ns sentry-theme)
+(ns sentry-theme
+  (:require [sentry-clj.core :as sentry]
+            [config.core :refer [env]]
+            [raven.client :as raven]))
 
-(println "hello")
+(def DSN (:sentry-dns env))
+
+(sentry/init! DSN)
+
+(def constant-e
+  (ex-info
+   "Get user info error"
+   {:user-id 42}
+   (ex-info "Auth error"
+            {:token "...."}
+            (ex-info "HTTP error"
+                     {:method "POST"
+                      :url "https://api.site.com"}))))
+
+(comment 
+  (sentry/send-event {:throwable constant-e})
+  (raven/capture! DSN constant-e))
